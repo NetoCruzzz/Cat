107c6f114787ee45ca26b5c4c473c626e02f82ba
Ernesto Cruz <161363066+NetoCruzzz@users.noreply.github.com>
2024-09-09 16:01:41 -0500

    Update Cat.h

--- Cat.h
+++ Cat.h
@@ -9,9 +9,24 @@
 
 // Access the first cat's name and set it to "Samward"
 
+	void speak2() {
+    cout << "meoww." << endl;
+  }
+
 class Cat {
   public:
+  // attributes (what describes what these objects are?)
       string name;
       int age;
       bool isSleepy;
+      
+      // methods (what can objects of this class do?)
+      	// define a method speak() that will print
+        // a message from the cat
+      // modify: when calling speak, print the name of the cat
+      	// Ex: speak for "Samward" ==> "Samward says: meow."
+      void speak() {
+        cout << name << " says: meow." << endl;
+      }
+      
 };
