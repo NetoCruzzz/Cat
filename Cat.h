ccd9f5a42339fac7fc72b137c5985b7c27567b42
Ernesto Cruz <161363066+NetoCruzzz@users.noreply.github.com>
2024-09-09 15:38:33 -0500

    Create Cat.h

---
+++ Cat.h
@@ -0,0 +1,10 @@
+//Define a class Cat with the following attributes:
+// name
+// age (whole number)
+// isSleepy (true or false)
+
+// Create a main functino in another file
+// import your lass definition
+// create two Cat objects
+
+// Access the first cat's name and set it to "Samward"
